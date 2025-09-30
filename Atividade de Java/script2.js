3656e70c248e1cd8da27000d45f084d72a36e9a5
Ziguiriguidum <heit.barreto.09@gmail.com>
2025-09-30 11:41:32 -0300

    Add files via upload

---
+++ Atividade de Java/script2.js
@@ -0,0 +1,6 @@
+//arrays -> vetores, matrizes
+//estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maça", "Melancia"]; //array de tamanho 3
+console.log(frutas);
+console.log(frutas[1])
